c82f171ff2088d7d2d9b331808d8c3684e42839c
jcd9 <jdavis@chicagotechacademy.org>
2015-09-18 10:23:12 -0500

    Create assignment4.rb

---
+++ assignment4.rb
@@ -0,0 +1,13 @@
+puts "How many hours do you work?"
+
+hours = 40
+
+puts "How much do you make in a week?"
+
+money = 20
+
+puts "How many weeks did you work?"
+
+weeks = 10
+
+puts "You would make $" (hours*money*weeks)
